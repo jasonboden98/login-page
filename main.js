b92e7c13f1a6cabdc395e6db67798b7353b4863c
JasonMacbook <jason.boden2072@outlook.com>
2025-09-06 11:49:08 +1000

    basic functionality

---
+++ main.js
@@ -0,0 +1,22 @@
+//select element
+const startButton = document.getElementById('startLogin');
+const loginBox = document.getElementById('loginBox');
+
+//switch state
+startButton.addEventListener('click', () => {
+    startButton.classList.add('hidden');
+    loginBox.classList.remove('hidden');
+});
+
+//handle form submission
+document.getElementById('submitLogin').addEventListener('click', () => {
+    const user = document.getElementById('username').value;
+    const pass = document.getElementById('password').value;
+
+    if (user && pass) {
+        alert('logging in as ${user}');
+    }
+    else {
+        alert('Please fill in fields');
+    }
+});
