486035c7b960881405e53458576c5129ebf5c042
JasonMacbook <jason.boden2072@outlook.com>
2025-09-07 13:17:08 +1000

    janky but working

--- main.js
+++ main.js
@@ -3,6 +3,17 @@ const startButton = document.getElementById('startLogin');
 const loginBox = document.getElementById('loginBox');
 
 //switch state
+//fucking this up, cant figure it out, moving on
+//function fadeOutElement(loginBox) {
+//    const element = document.getElementById(loginBox);
+//    if (element) {
+//        element.classList.add('fade-out');
+//        element.addEventListener('transitioned', () => {
+//            element.classList.add('hidden');
+//        }, {once: true});
+//    }
+//}
+
 startButton.addEventListener('click', () => {
     startButton.classList.add('hidden');
     loginBox.classList.remove('hidden');
